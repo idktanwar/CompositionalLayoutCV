10c24ca6834b0f3a86676181c75008e90593f453
Dinesh Tanwar <dtanwar@e-emphasys.com>
2021-06-20 12:38:14 +0530

    Compositional layout + carousel layout

--- CompositionalLayoutCV/ViewController.swift
+++ CompositionalLayoutCV/ViewController.swift
@@ -11,9 +11,94 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
 
+    func setupCollectionView() {
+        let collectionview = UICollectionView(frame: .zero, collectionViewLayout: getCompositionalLayout())
+        collectionview.delegate = self
+        collectionview.dataSource = self
+        collectionview.frame = self.view.frame
+        collectionview.backgroundColor = .white
+        collectionview.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        self.view.addSubview(collectionview)
+    }
+    
+    private func getCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        
+        ///Caraousal Layout
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        
+        
+       
+        ///Collectionview Compositional layout
+//        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)))
+//        item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+//
+//
+//        let group1Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)))
+//        group1Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+//
+//
+//        let nestedGroup1Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)))
+//        nestedGroup1Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+//        let nestedGroup1 = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)), subitems: [nestedGroup1Item1])
+//
+//        let group1 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [group1Item1, nestedGroup1])
+//
+//
+//        let group2Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .fractionalHeight(1)))
+//        group2Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+//        let group2 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [group2Item1])
+//
+//
+//        let containerGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(600)), subitems: [item, group1, group2])
+//
+//        let section = NSCollectionLayoutSection(group: containerGroup)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        let imageView = UIImageView(frame: cell.frame)
+//        imageView.image = UIImage(named: "\(indexPath.row)")
+//        imageView.contentMode = .scaleToFill
+//        cell.add(view: imageView, top: 0, left: 0, bottom: 0, right: 0)
+        
+        cell.backgroundColor = .random
+        return cell
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0.4...1), green: .random(in: 0.4...1), blue: .random(in: 0.4...1), alpha: 1)
+    }
+}
+
+extension UIView {
+    
+    func add(view: UIView, top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(view)
+        
+        view.topAnchor.constraint(equalTo: self.topAnchor, constant: top).isActive = true
+        view.leftAnchor.constraint(equalTo: self.leftAnchor, constant: left).isActive = true
+        view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: bottom).isActive = true
+        view.rightAnchor.constraint(equalTo: self.rightAnchor, constant: right).isActive = true
+    }
 }
 
